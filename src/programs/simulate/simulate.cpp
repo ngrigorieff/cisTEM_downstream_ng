ba46745d2e508566a25957389a5b98308a0ecf5f
Benjamin Himes <himes.benjamin@protonmail.com>
2020-12-18 16:28:26 -0500

    check 0,5

--- src/programs/simulate/simulate.cpp
+++ src/programs/simulate/simulate.cpp
@@ -11,7 +11,7 @@
 const float xtra_shift = -0.5f;
 const float HYDRATION_VALS[8] = {0.1750f,   -0.1350f,    2.23f,    3.43f,    4.78f,    1.0000f,    1.7700f,    0.9550f};
 const float DISTANCE_INIT = 100000.0f; // Set the distance slab to a large value
-const float BfactorFactor = 2.f;
+const float BfactorFactor = 0.5f;
 const int N_WATER_TERMS = 40;
 
 const float WATER_BFACTOR_PER_ELECTRON_PER_SQANG = 34.0f;
